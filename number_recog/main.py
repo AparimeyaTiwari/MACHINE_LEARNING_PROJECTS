7a2684b18c95758930a2203c3c9e9ed69d3cc0b7
AparimeyaTiwari <tiwariaparimeya@gmail.com>
2025-11-01 12:48:56 +0530

    number recogn

---
+++ number_recog/main.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+
+(X_train,y_train), (X_test,y_test) = keras.datasets.mnist.load_data()
+
+print(len(X_train))
+print(len(X_test))
